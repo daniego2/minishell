f4573d3ed55b5268c142ca6e24a0da13d0fcbd7d
daniego2 <danigonzalez282@gmail.com>
2025-04-19 01:53:13 +0200

    prompt now changes according to PWD. Protected cd from user unseting HOME. Mem leaks OK

--- src/built-ins/cd.c
+++ src/built-ins/cd.c
@@ -1,19 +1,39 @@
 #include "minishell.h"
 
+void update_pwd(t_env **env, char *newpwd)
+{
+    char *path;
+    char *temp;
+    t_env *pwd;
+
+    path = getcwd(NULL, 0);
+    if (path == NULL)
+        return;
+    pwd = get_environment_variable(*env, "PWD");
+    free(pwd->value);
+    pwd->value = ft_strdup(path);
+    free(path);
+}
+
+
 int exec_cd(t_env **env, char **argv)
 {
+    t_env *home_node;
+
+    home_node = get_environment_variable(*env, "HOME");
+
     if (!argv[1])
     {
-		//getenv() NO EXISTE EN EL PROYECTO
-        if (chdir(getenv("HOME")) != 0)
+        if (home_node == NULL)
         {
-            perror("cd: No Home directory");
+            printf("minishell: cd: HOME not set\n");
             return (1);
         }
+        else
+            chdir(home_node->value);
     }
     else
-	{
 		chdir(argv[1]);
-	}
+    update_pwd(env, argv[1]);
     return (0);
 }
