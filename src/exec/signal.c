55bb9d1566bba530d4f3fa24269370833c7a0e09
Daniel Gonzalez Sanchez <daniego2@c1r6s2.42malaga.com>
2025-05-08 21:41:36 +0200

    exit and signal hotfix

--- src/exec/signal.c
+++ src/exec/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniego2 <daniego@student.42.fr>           +#+  +:+       +#+        */
+/*   By: daniego2 <daniego2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 19:20:15 by daniego2          #+#    #+#             */
-/*   Updated: 2025/05/07 23:08:13 by daniego2         ###   ########.fr       */
+/*   Updated: 2025/05/08 19:06:42 by daniego2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,15 @@ extern int	g_signal;
 
 void	ctrl_c_handler(int sig)
 {
-	if (g_signal > 0)
-	{
-		(void)sig;
-		write(1, "\n", 1);
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		if (g_signal != 4)
-			rl_redisplay();
-		g_signal = 2;
-	}
+
+	(void)sig;
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	write(1, "\n", 1);
+	if (g_signal != 4)
+		rl_redisplay();
+	g_signal = 2;
+	
 	return ;
 }
 
